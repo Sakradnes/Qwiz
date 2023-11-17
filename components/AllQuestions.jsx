132fc71a2588831bcdd95a6e93404dec32fcdca7
Alex <sakradnes@mail.ru>
2023-11-17 15:13:21 +0300

    prereliz2

--- components/AllQuestions.jsx
+++ components/AllQuestions.jsx
@@ -8,12 +8,12 @@ function AllQuestion({ title , questions , user}){
 return ( 
     <Layout title={title}>  
 
-        <div className="cont" id='MainQuestion'>
-           { user && <div>ПРИВЕТСВУЕМ ТЕБЯ, {user.name}</div>}
+        <div className="container" id='MainQuestion'>
+           { user && <div className='h2Auth'>ПРИВЕТСВУЕМ ТЕБЯ, {user.name}</div>}
              <Question key={questions[0].id} question={questions[0]} user={user}/>
              <div className='free'></div>
              <Answer key={questions[0].id} question={questions[0]} user={user}/>
-        </div> 
+        </div>
     </Layout>
  )
 }
--- components/Answer.jsx
+++ components/Answer.jsx
@@ -7,16 +7,16 @@ module.exports = function Answer({ question, user }) {
         <p className="h2Auth">Введите Ваш ответ</p>
 
         <input
-          className="inputAnswer"
+          className="inputAnswer input"
           data-id={question.category_id}
           data-idi={question.id}
           data-udi={user.id}
           placeholder="ваш ответ сюда"
-        />
+        /><p className='dop'>scores:</p>
         <button className="yourScore">{user.score}</button>
 
         <div className="Compare"></div>
-        <div>Cложность вопроса:</div>
+        <div className='dop'>Cложность вопроса:</div>
         <button className="Dif">{question.difficulty}</button>
       </div>
     </div>
